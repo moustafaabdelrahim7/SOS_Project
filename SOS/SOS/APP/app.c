6dcdb95bc7f1e97d6cd078fdc3c59d371f23651e
moustafaabdelrahim7 <128411699+moustafaabdelrahim7@users.noreply.github.com>
2023-06-02 19:25:38 +0300

    Add files via upload

---
+++ SOS/SOS/APP/app.c
@@ -0,0 +1,118 @@
+/*
+ * app.c
+ *
+ * Created: 5/30/2023 7:43:50 AM
+ *  Author: Mcs
+ */ 
+#include "app.h"
+#include "../SERVICE/sos/sos_config.h"
+#include "../HAL/led/led_config.h"
+#include "../STD_LIBRARIES/ex_int_manger/ex_int_manger_config.h"
+
+
+
+static Std_ReturnType enu_sg_ret;
+volatile static uint16_t u8_sg_counter;
+
+
+static void Start_sos_handler(void)
+{
+	Start_Flag=1;
+}
+
+static void Stop_sos_handler(void)
+{
+	Start_Flag=2;
+}
+
+
+static void task_1_handler(void)
+{
+	LED_turn_toggle(&st_g_led0_instance);	
+}
+
+
+
+static void task_2_handler(void)
+{	
+	LED_turn_toggle(&st_g_led1_instance);
+	u8_sg_counter++;
+}
+
+void APP_init()
+{
+	GLOBAL_InterruptEnable();
+	
+	enu_sg_ret |=SOS_init();
+	if(SOS_E_NOK==enu_sg_ret)
+	{
+		// handle TASK fail initialization
+	}
+	enu_sg_ret = LED_initialize(&st_g_led0_instance);
+	if(E_NOT_OK==enu_sg_ret)
+	{
+		// handle led fail initialization
+	}
+	enu_sg_ret |= LED_initialize(&st_g_led1_instance);
+	if(E_NOT_OK==enu_sg_ret)
+	{
+		// handle led fail initialization
+	}
+	str_g_task_1_.Fptr_task_callBack = task_1_handler;
+	enu_sg_ret |=SOS_create_task(&str_g_task_1_);
+	if(SOS_E_NOK==enu_sg_ret)
+	{
+		// handle TASK fail initialization
+	}
+	str_g_task_2_.Fptr_task_callBack = task_2_handler;
+	enu_sg_ret |=SOS_create_task(&str_g_task_2_);
+	if(SOS_E_NOK==enu_sg_ret)
+	{
+		// handle TASK fail initialization
+	}
+/********************************************External interrupt for start & stop SOS********************************************************************/
+       enu_sg_ret|= EXT_INTx_setCallBack_manger(&Start_sos_handler,INTERRUPT_EXTERNAL_INT0);
+	   if (enu_sg_ret==E_NOT_OK)
+	   {
+		   // handle External interrupt fail set callback function 
+	   }
+	   else
+	   {
+		   //do nothing
+	   }
+       enu_sg_ret|= EXT_INTx_Init_manger(&s_INT0);
+	    if (enu_sg_ret==E_NOT_OK)
+	    {
+		    // handle External interrupt fail initialization
+	    }
+		else
+		{
+			//do nothing
+		}
+		enu_sg_ret|= EXT_INTx_setCallBack_manger(&Stop_sos_handler,INTERRUPT_EXTERNAL_INT1);
+		if (enu_sg_ret==E_NOT_OK)
+		{
+			// handle External interrupt fail set callback function
+		}
+		else
+		{
+			//do nothing
+		}
+		enu_sg_ret|= EXT_INTx_Init_manger(&s_INT1);
+		if (enu_sg_ret==E_NOT_OK)
+		{
+			// handle External interrupt fail initialization
+		}
+		else
+		{
+			//do nothing
+		}
+
+}
+
+void APP_start()
+{	
+		SOS_run();
+}
+
+
---
+++ SOS/SOS/main.c
@@ -0,0 +1,54 @@
+/*
+ * SOS.c
+ *
+ * Created: 5/30/2023 2:21:23 AM
+ * Author : Mcs
+ */ 
+
+//#include <avr/io.h>
+#include "APP/app.h"
+
+
+
+
+//#include <util/delay.h>
+#define  F_CPU 8000000UL
+
+
+
+int main(void)
+{
+	APP_init();
+	APP_start();
+
+}
+
+
+
+
+
+/*
+    / Replace with your application code 
+	APP_init();
+// 	GLOBAL_InterruptEnable();
+// 	LED_initialize(&st_g_led0_instance);
+// 	st_g_sos_timer.callBackFunction = SOS_timer_handelr2;
+// 	TIMER_Manager_init(&st_g_sos_timer);
+// 	TIMER_Manager_start(&st_g_sos_timer);
+	APP_start();
+	
+
+	
+
+/*_delay_ms(1000);
+    while (1) 
+    {
+		
+// 		 LED_turn_off(&st_g_led0_instance);
+// 		 LED_turn_off(&st_g_led1_instance);
+/*
+
+		LED_turn_toggle(&st_g_led0_instance);
+    }
+	*/
+
